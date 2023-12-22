2c39f14f64e58db9f20905ccc5b6e5c411078c8d
Your Name <you@example.com>
2023-12-22 11:26:06 +0530

    routes changes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -9,6 +9,7 @@ router.route("/updatePost/:postId").put(verifyAuth, postController.updatePost);
 router.route("/getSinglePost/:postId").get(verifyAuth, postController.getSinglePost);
 router.route("/getAllPosts/:userId").get(verifyAuth, postController.getAllPost);
 router.route("/removePost/:postId").delete(verifyAuth, postController.removeSinglePost);
+//
 router.route("/activeInactivePost/:userId").get(verifyAuth, postController.getAllActiveAndInactivePost);
 
 
